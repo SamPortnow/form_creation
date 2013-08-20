c0276dfe0c2337e0f693355c1f570ea90d6ff4cf
SamPortnow <samson91787@gmail.com>
2013-08-19 23:35:06 -0400

    added extra field to input box

--- static/main.js
+++ static/main.js
@@ -46,21 +46,36 @@ ko.bindingHandlers.jqButton = {
 
 
 
-var Product = function(id, text, isNew) {
+var Part = function(id, text, width, height, fontSize, isNew) {
+
     this.id = ko.observable(id);
     this.text = ko.observable(text);
+    this.width = ko.observable(width);
+    this.height = ko.observable(height);
+    this.fontSize = ko.observable(fontSize);
     this.isNew = isNew;
+
     this.editId = ko.observable(id);
     this.editText = ko.observable(text);
+    this.editWidth = ko.observable(width);
+    this.editHeight = ko.observable(height);
+    this.editFontSize = ko.observable(fontSize);
 
     //persist edits to real values on accept
     this.accept = function() {
-        this.id(this.editId()).text(this.editText());
+        this.id(this.editId()).
+            text(this.editText()).
+            width(this.editWidth()).
+            height(this.editHeight()).
+            fontSize(this.editFontSize());
     }.bind(this);
 
     //reset to originals on cancel
     this.cancel = function() {
-        this.editId(this.id()).editText(this.text());
+        this.editId(this.id()).
+            editText(this.text()).
+            editWidth(this.width()).
+            editHeight(this.height());
     }.bind(this);
 }
 
@@ -69,32 +84,32 @@ var ViewModel = function() {
     this.partList = ko.observableArray([
     ]);
 
-    this.selectedProduct = ko.observable();
-    this.editProduct = function(productToEdit) {
-        self.selectedProduct(productToEdit);
+    this.selectedPart = ko.observable();
+    this.editPart = function(PartToEdit) {
+        self.selectedPart(PartToEdit);
     };
     this.addPart = function() {
-        self.selectedProduct(new Product(0, "", true));
+        self.selectedPart(new Part("", "", "", "", "", true));
     },
-    this.removeProduct = function(product) {
-        self.partList.remove(product);
+    this.removePart = function(Part) {
+        self.partList.remove(Part);
     },
     this.accept = function() {
-        var selected = self.selectedProduct();
+        var selected = self.selectedPart();
         selected.accept();
 
         if (selected.isNew) {
-             self.partList.push(new Product(
-                 selected.id(), selected.text()));
+             self.partList.push(new Part(
+                 selected.id(), selected.text(), selected.width(), selected.height(), selected.fontSize()));
             $(".part").draggable();
 
         }
 
-        self.selectedProduct("");
+        self.selectedPart("");
     },
     this.cancel = function() {
-        self.selectedProduct().cancel();
-        self.selectedProduct("");
+        self.selectedPart().cancel();
+        self.selectedPart("");
     }
 };
 
